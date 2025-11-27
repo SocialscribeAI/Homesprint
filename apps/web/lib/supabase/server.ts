db0fd83a28d7eff8076e022e4f02dca50f5c4044
boruch crane <Baruch@webfused.dev>
2025-11-27 13:40:10 +0200

    fix database structure

--- apps/web/lib/supabase/server.ts
+++ apps/web/lib/supabase/server.ts
@@ -1,9 +1,10 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
+import { Database } from './types'
 
 export function createClient(cookieStore: ReadonlyRequestCookies) {
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
@@ -26,4 +27,3 @@ export function createClient(cookieStore: ReadonlyRequestCookies) {
     }
   )
 }
-
